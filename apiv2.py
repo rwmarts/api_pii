d76a9394d163a5f388e115df9a2f437c9ee6fae5
Richard Arts <richard@dqna.com>
2017-05-15 13:16:47 +0200

    Further improvements / additions to the api

--- apiv2.py
+++ apiv2.py
@@ -1,4 +1,8 @@
-from flask import Flask, request, jsonify
+import json
+import flask
+import httplib2
+from apiclient import discovery
+from oauth2client import client
 from flask_restful import Resource, Api, abort
 from mysqlfnc import connect, run_query
 from collections import OrderedDict
@@ -11,15 +15,16 @@ def bit_to_bool(value):
     return value > 0
 
 class PiiApi(Resource):
-    def fetch_result(self, query):
+    def execute_query(self, query):
         con = connect('localhost', 3306, 'yoda', 'dagobah', 'pii_db')
         cursor = run_query(con, query)
+        con.commit()
         con.close()
         return cursor
 
     def map_query(self, query, mapping, allow_empty_set=True):
         result = []
-        cursor = self.fetch_result(query)
+        cursor = self.execute_query(query)
         rows = cursor.fetchall()
         if len(rows) == 0 and not allow_empty_set:
             abort(404, message='No resource found')
@@ -42,6 +47,12 @@ class PiiApi(Resource):
         }
         return jsonify(response)
 
+
+    def id_exists(self, table, field, value):
+        query = "select count(*) from {} where {} = '{}'".format(table, field, value)
+        c = self.execute_query(query)
+        rows = c.fetchall()
+        return len(rows) == 1
 
     def get(self):
         return 'This is the API for the PII Tool'
@@ -152,13 +163,100 @@ class Schedules(PiiApi):
         mail_mapping = ['value', 'label', 'selected']
         for schedule in schedules:
             user_query = 'select users_id, email, active from users where user_types_id = 2 and users_id in (select users_id from users_schedules where schedules_id = {})'.format(schedule['scheduleId'])
-            print user_query
             users = self.map_query(user_query, mail_mapping)
-            schedule['emails'] = users
             for user in users:
                 user['selected'] = bit_to_bool(user['selected'])
+            schedule['emails'] = users
         return self.send_response(schedules)
 
+class ScheduleEdit(PiiApi):
+    def get(self):
+        return "Error: Incorrect use"
+
+    def post(self):
+        data = request.get_json(force=True)
+        if self.validate_fields(data):
+            if 'scheduleId' in data:
+                response = self.update_schedule(data)
+            else:
+                response = self.create_schedule(data)
+        else:
+            response = {"success": False}
+        return self.send_response(response)
+
+    def create_schedule(self, fields):
+        query = "insert into schedules(providers_id, frequencies_id, client_domain_id, expires, hash, active) values ({}, {}, {}, now(), '', 1)".format(fields['providerId'], fields['frequencyId'], fields['domainId'])
+        c = self.execute_query(query)
+        self.add_emails(c.lastrowid, fields['emailIds'])
+        response = {"success": True}
+        return response
+
+    def update_schedule(self, fields):
+        query = "update schedules set providers_id = {}, frequencies_id = {}, client_domain_id ={} where schedules_id = []".format(fields['providerId'], fields['frequencyId'], fields['domainId'], fields['scheduleId'])
+        self.remove_emails(fields['scheduleId'])
+        self.add_emails(fields['scheduleId'], fields['emailIds'])
+        response = {"success": True}
+        return response
+
+
+    def validate_fields(self, fields):
+        """
+        The following fields exist for the schedule call:
+        scheduleId: int (optional)
+        providerId: int
+        frequencyId: int
+        domainId: int
+        emailIds: list of ints
+        A field is valid when (a) the type check succeeds (b) it is an existing entry
+        TODO: Clean up
+        """
+        # Treat emailIds later, this is a list
+        valid = True
+        if 'scheduleId' in fields:
+            valid = type(fields['scheduleId']) is int
+            if valid:
+                valid = valid and self.id_exists('schedules', 'schedules_id', fields['scheduleId'])
+        valid = valid and type(fields['providerId']) is int
+        if valid:
+            valid = valid and self.id_exists('providers', 'providers_id', fields['providerId'])
+        valid = valid and type(fields['frequencyId']) is int
+        if valid:
+            valid = valid and self.id_exists('frequencies', 'frequencies_id', fields['frequencyId'])
+        valid = valid and type(fields['domainId']) is int
+        if valid:
+            valid = valid and self.id_exists('client_domains', 'client_domains_id', fields['domainId'])
+
+        # Check Email Ids
+        for id in fields['emailIds']:
+            valid = valid and id is int
+            if valid:
+                valid = valid and self.id_exists('users', 'users_id', id)
+
+        return valid
+
+
+    def add_emails(self, scheduleId, emails):
+        for email in emails:
+            query = "insert into user_schedules(schedules_id, users_id) values ({}, {})".format(
+                scheduleId, email)
+            self.execute_query(query)
+
+
+
+    def remove_emails(self, scheduleId):
+        query = "delete from user_schedules where schedules_id = {}".format(scheduleId)
+        self.execute_query(query)
+
+
+class ScheduleDelete(PiiApi):
+
+    def get(self, scheduleId):
+        # Start off naive
+        # TODO: scrub associative tables
+        query = "delete from schedules where schedules_id = {}".format(scheduleId)
+        self.execute_query(query)
+        response = {"success": True}
+        return response
 
 
 api.add_resource(PiiApi, '/api')
@@ -173,6 +271,79 @@ api.add_resource(DomainMailList, '/emails/<int:domain_id>')
 api.add_resource(ProviderClientList, '/provider-clients/<int:provider_id>')
 api.add_resource(ProviderMetricsList, '/provider-metrics/<int:provider_id>')
 api.add_resource(Schedules, '/schedules')
+api.add_resource(ScheduleEdit, '/schedule')
+api.add_resource(ScheduleDelete, '/schedule/delete/<int:schedule_id>')
+
+
+# Get a starting point for our tests
+# Flask routes
+
+def authorize(user):
+    authenticated = False
+    emails = user['emails']
+    for email in emails:
+        sql = 'select users_id, user_types_id from users where email = "{}"'
+        c = execute_query(sql)
+        rows = c.fetchall()
+        if len(rows) == 1:
+            row = rows[0]
+            flask.session['userid'] = row[0]
+            flask.session['usertype'] = row[1]
+            flask.session['email'] = email
+            authenticated = True
+            break;
+    if not authenticated:
+        flask.session.clear()
+    return authenticated
+
+
+
+def execute_query(self, query):
+    con = connect('localhost', 3306, 'yoda', 'dagobah', 'pii_db')
+    cursor = run_query(con, query)
+    con.commit()
+    con.close()
+    return cursor
+
+
+@application.route('/login')
+def login():
+  if 'credentials' not in flask.session:
+    return flask.redirect(flask.url_for('oauth2callback'))
+  credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+  if credentials.access_token_expired:
+    return flask.redirect(flask.url_for('oauth2callback'))
+  else:
+    http_auth = credentials.authorize(httplib2.Http())
+    plus = discovery.build('plus', 'v1', http_auth)
+    people = plus.people()
+    me = people.get(userId='me').execute()
+    authenticated = authorize(me)
+    if authenticated:
+        return flask.redirect(flask.url_for('index'))
+    return flask.redirect(flask.url_for('forbidden'))
+
+
+@application.route('/oauth2callback')
+def oauth2callback():
+  flow = client.flow_from_clientsecrets(
+      'client_secrets.json',
+      scope='https://www.googleapis.com/auth/plus.me',
+      redirect_uri=flask.url_for('oauth2callback', _external=True),
+      )
+  if 'code' not in flask.request.args:
+    auth_uri = flow.step1_get_authorize_url()
+    return flask.redirect(auth_uri)
+  else:
+    auth_code = flask.request.args.get('code')
+    credentials = flow.step2_exchange(auth_code)
+    flask.session['credentials'] = credentials.to_json()
+    return flask.redirect(flask.url_for('index'))
+
+@application.route("/tests")
+def testpage():
+    return render_template('page.html')
+
 
 if __name__ == '__main__':
     application.run(debug=True)
